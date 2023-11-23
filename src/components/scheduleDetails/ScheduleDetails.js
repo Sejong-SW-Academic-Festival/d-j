7e4321e372b35bfd2ca1ca6dc621d2d3e7a9983c
wlgh1553 <112055561+wlgh1553@users.noreply.github.com>
2023-11-23 20:21:55 +0900

    feat: 하트 애니메이션 적용

--- src/components/scheduleDetails/ScheduleDetails.js
+++ src/components/scheduleDetails/ScheduleDetails.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./ScheduleDetails.css";
 function ScheduleDetails({ monthlySchedules, selDate, colors }) {
   const toShowSchedules = monthlySchedules.filter(
@@ -7,58 +7,23 @@ function ScheduleDetails({ monthlySchedules, selDate, colors }) {
       new Date(e.endDate).getDate() >= selDate.getDate()
   );
 
-  //관심목록 받아오게 되면 여기 수정해줄것
+  // //관심목록 받아오게 되면 여기 수정해줄것
+  const initDictionary = toShowSchedules.map((e) => ({ [e.id]: false }));
+  const [heartSchedules, setHeartSchedules] = useState(initDictionary);
 
-  let [heartSchedules, toggleHeartSchedules] = useState(
-    new Array(toShowSchedules.length).fill(false)
-  );
-
-  toggleHeartSchedules = (event, index) => {
-    console.log("클릭!!", index);
-    heartSchedules[index] = !heartSchedules[index];
-  };
-
-  const unFilledHeart = (
-    <svg
-      className="heart-shape"
-      width="17"
-      height="14"
-      viewBox="0 0 17 14"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8.93916 12.8733C8.69833 12.9533 8.30166 12.9533 8.06083 12.8733C6.00666 12.2133 1.41666 9.46 1.41666 4.79333C1.41666 2.73333 3.18041 1.06667 5.355 1.06667C6.64416 1.06667 7.78458 1.65333 8.5 2.56C9.21541 1.65333 10.3629 1.06667 11.645 1.06667C13.8196 1.06667 15.5833 2.73333 15.5833 4.79333C15.5833 9.46 10.9933 12.2133 8.93916 12.8733Z"
-        stroke="#8F9BB3"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-  );
-  const filledHeart = (
-    <svg
-      className="heart-shape"
-      width="17"
-      height="14"
-      viewBox="0 0 17 14"
-      fill="#eb5050"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M8.93916 12.8733C8.69833 12.9533 8.30166 12.9533 8.06083 12.8733C6.00666 12.2133 1.41666 9.46 1.41666 4.79333C1.41666 2.73333 3.18041 1.06667 5.355 1.06667C6.64416 1.06667 7.78458 1.65333 8.5 2.56C9.21541 1.65333 10.3629 1.06667 11.645 1.06667C13.8196 1.06667 15.5833 2.73333 15.5833 4.79333C15.5833 9.46 10.9933 12.2133 8.93916 12.8733Z"
-        stroke="#eb5050"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      />
-    </svg>
-  );
+  function toggleHeartSchedules(scheduleID) {
+    const liked = heartSchedules[scheduleID];
+    setHeartSchedules((heartSchedules) => ({
+      ...heartSchedules,
+      [scheduleID]: !liked,
+    }));
+    console.log(heartSchedules);
+  }
 
   return (
     <div className="detail-body">
       <div className="details-container">
-        {toShowSchedules.map((e, index) => (
+        {toShowSchedules.map((e) => (
           <div key={e.id} className={"detail"}>
             <div className={`sign ${colors[e.category_id]}`}></div>
             <span className={`schedule-name ${colors[e.category_id]}`}>
@@ -66,9 +31,24 @@ function ScheduleDetails({ monthlySchedules, selDate, colors }) {
             </span>
             <div
               className="detail-like"
-              onClick={(e) => toggleHeartSchedules(e, index)}
+              onClick={() => toggleHeartSchedules(e.id)}
             >
-              {heartSchedules[index] ? filledHeart : unFilledHeart}
+              <svg
+                className={`heart-shape ${
+                  heartSchedules[e.id] ? "filled" : "unfilled"
+                }`}
+                width="17"
+                height="14"
+                viewBox="0 0 17 14"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.93916 12.8733C8.69833 12.9533 8.30166 12.9533 8.06083 12.8733C6.00666 12.2133 1.41666 9.46 1.41666 4.79333C1.41666 2.73333 3.18041 1.06667 5.355 1.06667C6.64416 1.06667 7.78458 1.65333 8.5 2.56C9.21541 1.65333 10.3629 1.06667 11.645 1.06667C13.8196 1.06667 15.5833 2.73333 15.5833 4.79333C15.5833 9.46 10.9933 12.2133 8.93916 12.8733Z"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
             </div>
           </div>
         ))}
